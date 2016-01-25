a2073890f449fe8ec23c793b475d65d1dda270ca
arcnmx <arcnmx@users.noreply.github.com>
2016-01-25 18:04:30 -0500

    encage-run arguments, runc subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,46 @@
 #[cfg(feature = "ocf")]
 extern crate ocf;
+#[macro_use]
+extern crate clap;
+
 extern crate encage_runtime as run;
 
 use std::process::exit;
+use clap::{App, AppSettings};
 
 fn main() {
-    #[cfg(feature = "ocf")]
-    fn run_ocf() {
-        let (spec, runtime) = ocf::load(".").expect("failed to load config");
-        let (config, cmd) = run::ocf_config(&spec, &runtime).expect("failed to process config");
-        let status = run::run(&config, &cmd).expect("failed to run");
-        exit(status);
-    }
+    let app = App::new("encage-run").setting(AppSettings::SubcommandRequiredElseHelp);
+    let app = clap_app! { @app (app)
+        (author: "arcnmx")
+        (about: "Encage Linux container runtime")
+    };
 
     #[cfg(not(feature = "ocf"))]
-    fn run_ocf() {
-        panic!("Not built with OCF support");
+    fn ocf_args<'a, 'b, 'c, 'd, 'e, 'f>(app: App<'a, 'b, 'c, 'd, 'e, 'f>) -> App<'a, 'b, 'c, 'd, 'e, 'f> { app }
+    #[cfg(feature = "ocf")]
+    fn ocf_args<'a, 'b, 'c, 'd, 'e, 'f>(app: App<'a, 'b, 'c, 'd, 'e, 'f>) -> App<'a, 'b, 'c, 'd, 'e, 'f> {
+        clap_app! { @app (app)
+            (@subcommand runc =>
+                (about: "Runs an OCF-compliant image")
+                (@arg PATH: "A directory containing the OCF image")
+            )
+        }
     }
+    let app = ocf_args(app);
+    let matches = app.get_matches();
 
-    run_ocf();
+    match matches.subcommand() {
+        #[cfg(feature = "ocf")]
+        ("runc", Some(matches)) => {
+
+            let path = matches.value_of("PATH");
+            let path = path.map(|s| &s[..]).unwrap_or(".");
+
+            let (spec, runtime) = ocf::load(path).expect("failed to load config");
+            let (config, cmd) = run::ocf_config(&spec, &runtime).expect("failed to process config");
+            let status = run::run(&config, &cmd).expect("failed to run");
+            exit(status);
+        },
+        _ => unreachable!()
+    }
 }
